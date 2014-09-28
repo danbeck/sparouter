6e6a68aacee91c2784120ee845283dcb1f4d7707
Daniel <d.danielbeck@googlemail.com>
2014-09-28 18:37:57 +0200

    Refactoring.

--- sparouter/public_html/sparouter.js
+++ sparouter/public_html/sparouter.js
@@ -19,33 +19,38 @@ window.sparouter = (function() {
 
 
     var sparouter = function(oldurl, newurl) {
-        if (oldurl === window.document) {
-            var selectedAHrefs = window.document.querySelectorAll("a[href^='#']");
-            var datapages = window.document.querySelectorAll("div[data-page]");
+        if (!sparouter.initialized) {
+            sparouter.initialized = true;
 
-            for (var i = 1; i < datapages.length; i++) {
-                datapages[i].style.display = "none";
-            }
+            makeAllPagesInvisible();
+            window.document.querySelectorAll("div[data-page]")[0].style.display = "block";
+
+            registerClickHandler();
+        }
+
+        function registerClickHandler() {
+            var selectedAHrefs = window.document.querySelectorAll("a[href^='#']");
             for (var j = 0; j < selectedAHrefs.length; j++) {
                 console.log("add Event handler");
                 selectedAHrefs[j].addEventListener("click", function(e) {
                     var url = this.getAttribute("href");
                     console.log("clicked on " + url);
-                    e.preventDefault();
+                    makeAllPagesInvisible();
                     url = url.substring(1);
-                    for (var i = 0; i < datapages.length; i++) {
-                        datapages[i].style.display = "none";
-                    }
-
                     var newPage = window.document.querySelector("div[data-page=" + url + "]");
                     newPage.style.display = "block";
 
                 });
             }
-
-            console.dir(selectedAHrefs);
-            console.dir(datapages);
         }
+
+        function makeAllPagesInvisible() {
+            var datapages = window.document.querySelectorAll("div[data-page]");
+            for (var i = 0; i < datapages.length; i++) {
+                datapages[i].style.display = "none";
+            }
+        }
+
         return new Sparouter(newurl, oldurl);
     };
     return sparouter;
